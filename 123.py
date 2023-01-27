a01b19ffa76d93bd55395303e5a77401663820c4
Artur <artur.ksteam@gmail.com>
2023-01-27 20:11:11 +0600

    adding files

--- 123.py
+++ 123.py
@@ -1,2 +1,16 @@
-for i in range(0, 10):
-    print('hello world')
+quiz = {
+    'Which is the currency of Kazakhstan?': ('tenge', 'kaz'),
+    'Name one of the past/present presidents of Kazakhstan': ('tokayev', 'nazarbayev'),
+    'What year Kazakhstan proclaim independence?': '1991'
+}
+cor_ans = 0
+for i in quiz.keys():
+    print(i)
+    ans = str(input())
+    if ans.lower() in quiz[i]:
+        cor_ans += 1
+        continue
+    else:
+        continue
+
+print(cor_ans)
